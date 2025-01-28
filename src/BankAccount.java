8519a3d40ed31f9c882de61b5f233ef5382cc517
Gautier Benoit <gautier.benoit@etu.univ-cotedazur.fr>
2025-01-28 10:36:52 +0100

    Add BankAccount class and Depositable/Withdrawable interfaces

---
+++ src/BankAccount.java
@@ -0,0 +1,20 @@
+public abstract class BankAccount implements Depositable {
+    protected double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Le montant du dépôt doit être positif");
+        }
+        this.balance += amount;
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+}
